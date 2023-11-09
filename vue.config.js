37408144a3dfb1ff8f73cfb1119c0596340bc686
陆思 <18924255765@163.com>
2023-11-09 09:49:16 +0800

    第三次

--- vue.config.js
+++
@@ -1,9 +0,0 @@
-
-// const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
-// const { extendDefaultPlugins } = require("svgo");
-// const CompressionPlugin = require("compression-webpack-plugin");
-module.exports = {
-    outputDir: 'projrct',
-    publicPath: './',
-    productionSourceMap: false
-}
